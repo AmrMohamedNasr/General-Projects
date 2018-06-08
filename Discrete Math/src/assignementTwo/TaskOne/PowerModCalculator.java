f83e3ef3d1812a5e936b494d4a6c70e686bae690
Amr Mohamed Nasr <re13amr@gmail.com>
2018-06-08 14:07:06 +0200

    Only Old web project in this repo

--- Discrete Math/src/assignementTwo/TaskOne/PowerModCalculator.java
+++
@@ -1,9 +0,0 @@
-package assignementTwo.TaskOne;
-
-import java.math.BigInteger;
-
-public interface PowerModCalculator {
-	public BigInteger calculateApowerBmodN(BigInteger a,
-			BigInteger b,
-			BigInteger n);
-}
